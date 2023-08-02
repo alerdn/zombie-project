28ef9bfc294450c779fd1390f98eb20aa70bfa12
rycsouza <rychard.souza1111@gmail.com>
2023-08-02 12:02:47 -0300

    fix: corrigindo pulo

--- Assets/Scripts/Core/Player/Player.cs
+++ Assets/Scripts/Core/Player/Player.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
-using Cinemachine;
+using UnityEngine.InputSystem.iOS;
 
 namespace ZombieProject.Core
 {
@@ -10,30 +11,46 @@ namespace ZombieProject.Core
     {
         public NetworkVariable<bool> IsShooting = new NetworkVariable<bool>();
 
-        [SerializeField] private float _moveSpeed = 25f;
+        [Header("Input")]
         [SerializeField] private InputReader _inputReader;
 
-        [Header("Camera")]
-        [SerializeField] private CinemachineVirtualCamera _vcam;
-        [SerializeField] private int _vcamPriority = 15;
+        [Header("Movement")]
+        [SerializeField] private CharacterController _characterController;
+        [SerializeField] private Vector3 _foward;
+        [SerializeField] private Vector3 _strafe;
+        [SerializeField] private Vector3 _vertical;
+        [SerializeField] private float _fowardSpeed = 5f;
+        [SerializeField] private float _strafeSpeed = 5f;
+        [SerializeField] private Transform _groundCheckTransform;
+        [Header("Jump")]
+        [SerializeField] private float _gravity;
+        [SerializeField] private float _jumpSpeed;
+        [SerializeField] private float _jumpMaxHeight = 2f;
+        [SerializeField] private float _timeToMaxHeight = .5f;
+        [SerializeField] private bool _isGrounded;
+        [SerializeField] private LayerMask _groundLayers;
 
-        private Vector2 _movementDirection;
-        private CharacterController _charController;
-        private float _turnSmoothVelocity;
-        private Transform _cam;
-        private string userAuthId;
-        private GameInfo userGamePreferences;
+        [Header("Vision")]
+        [SerializeField] private Transform _viewPoint;
+        [SerializeField] private Vector2 _mouseInput;
+        [SerializeField] private float _mouseSensitivity = 5f;
+        [SerializeField] private float _verticalRotStore;
+        [SerializeField] private Camera _cam;
+        [SerializeField] private bool InvertLook = false;
 
         public override void OnNetworkSpawn()
         {
             if (!IsOwner) return;
 
-            _charController = GetComponent<CharacterController>();
-            _cam = Camera.main.transform;
+            Cursor.lockState = CursorLockMode.Locked;
 
-            _vcam.Priority = _vcamPriority;
+            _characterController = GetComponent<CharacterController>();
 
-            _inputReader.MovementEvent += HandleMovementInputs;
+            _cam = Camera.main;
+
+            _gravity = (-2 * _jumpMaxHeight) / (_timeToMaxHeight * _timeToMaxHeight);
+            _jumpSpeed = (2 * _jumpMaxHeight) / _timeToMaxHeight;
+
             _inputReader.ShootEvent += HandleShootInputServerRpc;
         }
 
@@ -41,7 +58,6 @@ namespace ZombieProject.Core
         {
             if (!IsOwner) return;
 
-            _inputReader.MovementEvent -= HandleMovementInputs;
             _inputReader.ShootEvent -= HandleShootInputServerRpc;
         }
 
@@ -50,49 +66,67 @@ namespace ZombieProject.Core
             if (!IsOwner) return;
 
             HandleMovement();
+            HandleJump();
+            HandleVision();
         }
 
-        private void HandleMovementInputs(Vector2 direction)
+        private void LateUpdate()
         {
-            _movementDirection = direction;
+            if (!IsOwner) return;
+
+            if (_cam == null) _cam = Camera.main;
+
+            _cam.transform.SetPositionAndRotation(_viewPoint.position, _viewPoint.rotation);
+        }
+
+        private void HandleMovement()
+        {
+            float forwardInput = Input.GetAxisRaw("Vertical");
+            float strafeInput = Input.GetAxisRaw("Horizontal");
+
+            _foward = forwardInput * _fowardSpeed * transform.forward;
+            _strafe = strafeInput * _strafeSpeed * transform.right;
+
+            Vector3 finalVelocity = _foward + _strafe + _vertical;
+
+            _characterController.Move(finalVelocity * Time.deltaTime);
+        }
+
+        private void HandleJump()
+        {
+            _vertical += _gravity * Time.deltaTime * Vector3.up;
+
+
+            if (_characterController.isGrounded)
+            {
+                _vertical = Vector3.down;
+            }
+
+            _isGrounded = Physics.Raycast(_groundCheckTransform.position, Vector3.down, .25f, _groundLayers);
+
+            if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+            {
+                _vertical = _jumpSpeed * Vector3.up;
+            }
+        }
+
+        private void HandleVision()
+        {
+            _mouseInput = new Vector2(Input.GetAxisRaw("Mouse X") * _mouseSensitivity, Input.GetAxisRaw("Mouse Y")) * _mouseSensitivity;
+
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + _mouseInput.x, transform.rotation.eulerAngles.z);
+
+            _verticalRotStore += _mouseInput.y;
+            _verticalRotStore = Mathf.Clamp(_verticalRotStore, -60f, 60f);
+
+            if (InvertLook) _viewPoint.rotation = Quaternion.Euler(_verticalRotStore, _viewPoint.rotation.eulerAngles.y, _viewPoint.rotation.eulerAngles.z);
+            else _viewPoint.rotation = Quaternion.Euler(-_verticalRotStore, _viewPoint.rotation.eulerAngles.y, _viewPoint.rotation.eulerAngles.z);
         }
 
         [ServerRpc]
         private void HandleShootInputServerRpc(bool isShooting)
         {
             IsShooting.Value = isShooting;
-        }
-
-        private void HandleMovement()
-        {
-            float horizontal = _movementDirection.x;
-            float vertical = _movementDirection.y;
-
-            if (_cam == null) _cam = Camera.main.transform;
-
-            float targetAngle = Mathf.Atan2(horizontal, vertical) * Mathf.Rad2Deg + _cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, .1f);
-            Vector3 speedVector = (Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward).normalized * _moveSpeed;
-
-            // Check if player is moving
-            bool isWalking = _movementDirection.magnitude >= .1;
-            if (isWalking)
-            {
-                transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            }
-            else
-            {
-                speedVector = Vector3.zero;
-            }
-
-            // Check if player is grounded
-            if (!_charController.isGrounded)
-            {
-                speedVector.y -= 9.87f * Time.deltaTime;
-            }
-
-            // Moving player
-            _charController.Move(speedVector * Time.deltaTime);
         }
     }
 }
