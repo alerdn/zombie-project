752c5e32406ea6c1d9ff7f27546180b02ef44481
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2023-08-02 13:44:36 -0400

    feat: previnindo criação de host enquanto procurando por partida

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
@@ -13,6 +15,8 @@ public class MainMenu : MonoBehaviour
 
     private bool _isMatchmaking;
     private bool _isCanceling;
+    private float _timeInQueue;
+    private bool _isBusy;
 
     private void Start()
     {
@@ -20,6 +24,16 @@ public class MainMenu : MonoBehaviour
 
         _queueStatusText.text = string.Empty;
         _queueTimerText.text = string.Empty;
+    }
+
+    private void Update()
+    {
+        if (_isMatchmaking)
+        {
+            _timeInQueue += Time.deltaTime;
+            TimeSpan ts = TimeSpan.FromSeconds(_timeInQueue);
+            _queueTimerText.text = string.Format("{0:00}:{1:00}", ts.Minutes, ts.Seconds);
+        }
     }
 
     public async void FindMatch()
@@ -30,21 +44,27 @@ public class MainMenu : MonoBehaviour
         {
             _queueStatusText.text = "Cancelling...";
             _isCanceling = true;
-            
+
             await ClientSingleton.Instance.GameManager.CancelMatchmaking();
 
             _isCanceling = false;
             _isMatchmaking = false;
+            _isBusy = false;
 
             _findMatchButtonText.text = "Find Match";
             _queueStatusText.text = string.Empty;
+            _queueTimerText.text = string.Empty;
             return;
         }
+
+        if (_isBusy) return;
 
         ClientSingleton.Instance.GameManager.MatchmakeAsync(OnMatchMade);
         _findMatchButtonText.text = "Cancel";
         _queueStatusText.text = "Searching...";
+        _timeInQueue = 0f;
         _isMatchmaking = true;
+        _isBusy = true;
     }
 
     private void OnMatchMade(MatchmakerPollingResult result)
@@ -71,11 +91,40 @@ public class MainMenu : MonoBehaviour
 
     public async void StartHost()
     {
+        if (_isBusy) return;
+        _isBusy = true;
+
         await HostSingleton.Instance.GameManager.StartHostAsync();
+        _isBusy = false;
     }
 
     public async void StartClient()
     {
+        if (_isBusy) return;
+        _isBusy = true;
+
         await ClientSingleton.Instance.GameManager.StartClientAsync(_joinCodeField.text);
+        _isBusy = false;
+    }
+
+    public async void JoinAsync(Lobby lobby)
+    {
+        if (_isBusy) return;
+
+        _isBusy = true;
+
+        try
+        {
+            Lobby joiningLobby = await Lobbies.Instance.JoinLobbyByIdAsync(lobby.Id);
+            string joinCode = joiningLobby.Data["JoinCode"].Value;
+
+            await ClientSingleton.Instance.GameManager.StartClientAsync(joinCode);
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+
+        _isBusy = false;
     }
 }
